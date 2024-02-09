3e7f1450745620842090dd520fab50089c690757
aneesahmed98 <120256209+aneesahmed98@users.noreply.github.com>
2024-02-09 08:49:39 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask 
+#To use templates we have to use render remplates
+from flask import render_template
 app = Flask(__name__) #creating an object
 #created application now creating a route
 @app.route("/") #when url / is accessed then run hello world
 def hello_world():
-  return "Hello World"
+  return render_template('home.html')
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
   
